c0b94387506eb2d2805f4c3895da25aee847409d
Mariana U <mariana.urrego@upb.edu.co>
2024-10-03 01:01:57 -0500

    feat:pagina about us, falta los css

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,50 @@
+<!-- resources/views/layouts/main.blade.php -->
+<!doctype html>
+<html lang="en">
+@include('partials.head') <!-- Referencia al archivo head.blade.php -->
+
+<body>
+
+    <div class="site-mobile-menu site-navbar-target">
+        <div class="site-mobile-menu-header">
+            <div class="site-mobile-menu-close">
+                <span class="icofont-close js-menu-toggle"></span>
+            </div>
+        </div>
+        <div class="site-mobile-menu-body"></div>
+    </div>
+
+    @include('partials.navbar')
+
+    <main>
+        @yield('content')<!-- Aquí se insertará el contenido específico de la página -->
+    </main>
+
+    <footer>
+        @include('partials.footer')
+    </footer>
+
+    <!-- Preloader -->
+    <div id="overlayer"></div>
+    <div class="loader">
+        <div class="spinner-border text-primary" role="status">
+            <span class="visually-hidden">Loading...</span>
+        </div>
+    </div>
+
+
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/tiny-slider.js"></script>
+
+    <script src="js/flatpickr.min.js"></script>
+
+
+    <script src="js/aos.js"></script>
+    <script src="js/glightbox.min.js"></script>
+    <script src="js/navbar.js"></script>
+    <script src="js/counter.js"></script>
+    <script src="js/custom.js"></script>
+
+</body>
+
+</html>
