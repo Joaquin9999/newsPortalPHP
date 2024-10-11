43195b540e67e32f66a7ec5c6ad3152fa366ea27
Mariana U <mariana.urrego@upb.edu.co>
2024-10-10 20:31:52 -0500

    chore: movemos componentes(controladores,vistas,rutas,modelos,assets) para modificar el dashboard

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -6,19 +6,21 @@
             <div class="site-navigation">
                 <div class="row g-0 align-items-center">
                     <div class="col-2">
-                        <a href="{{ route('home')}}" class="logo m-0 float-start">CiberTrends<span class="text-primary;">.</span></a>
+                        <a href="{{ route('home')}}" class="logo m-0 float-start">CiberTrends<span
+                                class="text-primary;">.</span></a>
                     </div>
                     <div class="col-8 text-center">
                         <ul class="js-clone-nav d-none d-lg-inline-block text-start site-menu mx-auto">
-                            <li><a href="{{ route('home')}}">Inicio</a></li>
-                            <li><a href="{{ route('category')}}">Ciberseguridad</a></li>
+                            <li><a href="{{ route('home')}}">Home</a></li>
+                            <li><a href="{{ route('category')}}">Cibersecurity</a></li>
                             <li><a href="{{ route('category')}}">Web</a></li>
                             <li><a href="{{ route('category')}}">Software</a></li>
                             <li><a href="{{ route('category')}}">I.A</a></li>
                         </ul>
                     </div>
                     <div class="col-2 d-flex align-items-center justify-content-end">
-                        <form action="#" class="search-form me-3" style="flex: 1; max-width: 250px; margin-left: -100px;">
+                        <form action="#" class="search-form me-3"
+                            style="flex: 1; max-width: 250px; margin-left: -100px;">
                             <input type="text" class="form-control" placeholder="Search..."
                                 style="width: 100%; font-size: 14px;">
                             <span class="bi-search"></span>
