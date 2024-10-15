2dd36f88c4a65d4c27818144cc579acbddf8fad8
Joaquin9999 <joaco.programmer@gmail.com>
2024-10-14 22:56:15 -0500

    Fix:cambio de peticiones para separar secciones de inicio

--- resources/views/pages/category.blade.php
+++ resources/views/pages/category.blade.php
@@ -53,74 +53,37 @@
                     <h3 class="heading">Popular Posts</h3>
                     <div class="post-entry-sidebar">
                         <ul>
-                            <li>
-                                <a href="">
-                                    <img src="images/img_1_sq.jpg" alt="Image placeholder" class="me-4 rounded">
-                                    <div class="text">
-                                        <h4>There’s a Cool New Way for Men to Wear Socks and Sandals</h4>
-                                        <div class="post-meta">
-                                            <span class="mr-2">March 15, 2018 </span>
+                            @php
+                                $popularPosts = \TCG\Voyager\Models\Post::inRandomOrder()->take(3)->get();
+                            @endphp
+
+                            @foreach ($popularPosts as $post)
+                                <li>
+                                    <a href="{{ route('single', $post->slug) }}">
+                                        <img src="{{ Voyager::image($post->image) }}" alt="Image placeholder"
+                                            class="me-4 rounded">
+                                        <div class="text">
+                                            <h4>{{ $post->title }}</h4>
+                                            <div class="post-meta">
+                                                <span class="mr-2">{{ $post->created_at->format('F j, Y') }}</span>
+                                            </div>
                                         </div>
-                                    </div>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="">
-                                    <img src="images/img_2_sq.jpg" alt="Image placeholder" class="me-4 rounded">
-                                    <div class="text">
-                                        <h4>There’s a Cool New Way for Men to Wear Socks and Sandals</h4>
-                                        <div class="post-meta">
-                                            <span class="mr-2">March 15, 2018 </span>
-                                        </div>
-                                    </div>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="">
-                                    <img src="images/img_3_sq.jpg" alt="Image placeholder" class="me-4 rounded">
-                                    <div class="text">
-                                        <h4>There’s a Cool New Way for Men to Wear Socks and Sandals</h4>
-                                        <div class="post-meta">
-                                            <span class="mr-2">March 15, 2018 </span>
-                                        </div>
-                                    </div>
-                                </a>
-                            </li>
+                                    </a>
+                                </li>
+                            @endforeach
                         </ul>
                     </div>
                 </div>
-                <!-- END sidebar-box -->
-
                 <div class="sidebar-box">
                     <h3 class="heading">Categories</h3>
                     <ul class="categories">
-                        <li><a href="#">Food <span>(12)</span></a></li>
-                        <li><a href="#">Travel <span>(22)</span></a></li>
-                        <li><a href="#">Lifestyle <span>(37)</span></a></li>
-                        <li><a href="#">Business <span>(42)</span></a></li>
-                        <li><a href="#">Adventure <span>(14)</span></a></li>
+                        <li><a href="#">Ciberseguridad <span>(12)</span></a></li>
+                        <li><a href="#">Web <span>(22)</span></a></li>
+                        <li><a href="#">Software <span>(37)</span></a></li>
+                        <li><a href="#">I.A <span>(42)</span></a></li>
                     </ul>
                 </div>
                 <!-- END sidebar-box -->
-
-                <div class="sidebar-box">
-                    <h3 class="heading">Tags</h3>
-                    <ul class="tags">
-                        <li><a href="#">Travel</a></li>
-                        <li><a href="#">Adventure</a></li>
-                        <li><a href="#">Food</a></li>
-                        <li><a href="#">Lifestyle</a></li>
-                        <li><a href="#">Business</a></li>
-                        <li><a href="#">Freelancing</a></li>
-                        <li><a href="#">Travel</a></li>
-                        <li><a href="#">Adventure</a></li>
-                        <li><a href="#">Food</a></li>
-                        <li><a href="#">Lifestyle</a></li>
-                        <li><a href="#">Business</a></li>
-                        <li><a href="#">Freelancing</a></li>
-                    </ul>
-                </div>
-
             </div>
         </div>
     </div>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,25 +1,30 @@
 @extends('layouts.main')
 
 @section('content')
-    <!-- Start retroy layout blog posts -->
-    <section class="section bg-light">
-        <div class="container">
-            <div class="row align-items-stretch retro-layout">
-                @foreach ($posts->slice(0, 6) as $post)
-                    <div class="col-md-4">
-                        <a href="{{ route('single', $post->slug) }}" class="h-entry mb-30 v-height gradient">
-                            <div class="featured-img" style="background-image: url('{{ Voyager::image($post->image) }}');">
-                            </div>
-                            <div class="text">
-                                <span class="date">{{ $post->created_at }}</span>
-                                <h2>{{ $post->title }}</h2>
-                            </div>
-                        </a>
-                    </div>
-                @endforeach
-            </div>
+<section class="section bg-light">
+    <div class="container">
+        <div class="row align-items-stretch retro-layout">
+            @php
+                // Obtener 6 posts aleatorios
+                $randomPosts = \TCG\Voyager\Models\Post::inRandomOrder()->take(6)->get();
+            @endphp
+
+            @foreach ($randomPosts as $post)
+                <div class="col-md-4">
+                    <a href="{{ route('single', $post->slug) }}" class="h-entry mb-30 v-height gradient">
+                        <div class="featured-img" style="background-image: url('{{ Voyager::image($post->image) }}');">
+                        </div>
+                        <div class="text">
+                            <span class="date">{{ $post->created_at->format('M. d, Y') }}</span>
+                            <h2>{{ $post->title }}</h2>
+                        </div>
+                    </a>
+                </div>
+            @endforeach
         </div>
-    </section>
+    </div>
+</section>
+
     <!-- End retroy layout blog posts -->
 
 <!-- Start posts-entry -->
@@ -27,7 +32,7 @@
     <div class="container">
         <div class="row mb-4">
             <div class="col-sm-6">
-                <h2 class="posts-entry-title">Business</h2>
+                <h2 class="posts-entry-title">Ciberseguridad    </h2>
             </div>
             <div class="col-sm-6 text-sm-end">
                 <a href="category.html" class="read-more">View All</a>
@@ -36,7 +41,7 @@
         <div class="row g-3">
             <div class="col-md-8"> <!-- Aumentar espacio para el post -->
                 <div class="row g-3">
-                    @foreach ($posts->slice(1, 1) as $post) <!-- Solo un post destacado -->
+                    @foreach ($posts->where('category_id', 1)->slice(1, 1) as $post) <!-- Solo un post destacado -->
                         <div class="col-md-12"> <!-- Usar col-md-12 para que ocupe toda la columna -->
                             <div class="blog-entry">
                                 <a href="{{ route('single', $post->slug) }}" class="img-link">
@@ -76,7 +81,7 @@
 <section class="section posts-entry posts-entry-sm bg-light">
     <div class="container">
         <div class="row">
-            @foreach ($posts->slice(0, 4) as $post) <!-- Muestra solo los primeros 4 posts -->
+            @foreach ($posts->where('category_id', 4)->slice(0, 4) as $post) <!-- Muestra solo los primeros 4 posts -->
                 <div class="col-md-6 col-lg-3">
                     <div class="blog-entry">
                         <a href="{{ route('single', $post->slug) }}" class="img-link">
@@ -98,14 +103,14 @@
     <div class="container">
         <div class="row mb-4">
             <div class="col-sm-6">
-                <h2 class="posts-entry-title">Culture</h2>
+                <h2 class="posts-entry-title">Web</h2>
             </div>
             <div class="col-sm-6 text-sm-end"><a href="category.html" class="read-more">View All</a></div>
         </div>
         <div class="row g-3">
             <div class="col-md-9 order-md-2">
                 <div class="row g-3">
-                    @foreach ($posts->slice(1, 2) as $post) <!-- Mostrar los primeros 3 posts -->
+                    @foreach ($posts->where('category_id', 2)->slice(1, 2) as $post) <!-- Mostrar los primeros 3 posts -->
                         <div class="col-md-6">
                             <div class="blog-entry">
                                 <a href="{{ route('single', $post->slug) }}" class="img-link">
@@ -122,7 +127,7 @@
             </div>
             <div class="col-md-3">
                 <ul class="list-unstyled blog-entry-sm">
-                    @foreach ($posts->slice(0, 3) as $post) <!-- Mostrar los siguientes 2 posts -->
+                    @foreach ($posts->where('category_id', 2)->slice(0, 3) as $post) <!-- Mostrar los siguientes 2 posts -->
                         <li>
                             <span class="date">{{ $post->created_at->format('M. d, Y') }}</span>
                             <h3><a href="{{ route('single', $post->slug) }}">{{ $post->title }}</a></h3>
@@ -142,14 +147,14 @@
 
         <div class="row mb-4">
             <div class="col-sm-6">
-                <h2 class="posts-entry-title">Politics</h2>
+                <h2 class="posts-entry-title">I.A</h2>
             </div>
             <div class="col-sm-6 text-sm-end"><a href="category.html" class="read-more">View All</a></div>
         </div>
 
         <div class="row">
             <div class="row">
-                @foreach ($posts->slice(0, 9) as $post)
+                @foreach ($posts->where('category_id', 3)->slice(0, 9) as $post)
                     <div class="col-lg-4 mb-4">
                         <div class="post-entry-alt">
                             <a href="{{ route('single', $post->slug) }}" class="img-link">
